a8352b86d9eba77bb931fe64d8a0cf6bc1b184a9
pftq <pftq@pftq.com>
2025-04-22 20:30:44 -0700

    Exposed negative_prompt option

--- generate_video_df.py
+++ generate_video_df.py
@@ -43,6 +43,7 @@ if __name__ == "__main__":
     
     parser.add_argument("--batch_size", type=int, default=1) # 20250422 pftq: Batch functionality to avoid reloading the model each video
     parser.add_argument("--preserve_image_aspect_ratio", action="store_true")  # 20250422 pftq: Avoid resizing
+    parser.add_argument("--negative_prompt", type=str, default="Bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards") # 20250422 pftq: expose negative prompt
     
     args = parser.parse_args()
 
@@ -127,7 +128,7 @@ if __name__ == "__main__":
             height = height_tensor.item()
             width = width_tensor.item()
 
-    negative_prompt = "色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走"
+    negative_prompt = args.negative_prompt # 20250422 pftq: allow editable negative prompt
 
     save_dir = os.path.join("result", args.outdir)
     os.makedirs(save_dir, exist_ok=True)
