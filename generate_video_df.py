8b88304e5354bebe167f7b0a709aa31880c2a252
pftq <pftq@pftq.com>
2025-04-28 14:18:04 -0700

    Integrated prompt travel and video input from chaojie and fixed merged conflicts.

--- generate_video_df.py
+++ generate_video_df.py
@@ -8,7 +8,7 @@ from diffusers.utils import load_image
 import imageio
 from PIL import Image  #20250422 pftq: Added for image resizing and cropping
 import numpy as np  #20250422 pftq: Added for seed synchronization
-from diffusers.utils import load_image, load_video
+from diffusers.utils import load_video # 20250425 chaojie prompt travel & video input
 
 from skyreels_v2_infer import DiffusionForcingPipeline
 from skyreels_v2_infer.modules import download_model
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     parser.add_argument("--resolution", type=str, choices=["540P", "720P"])
     parser.add_argument("--num_frames", type=int, default=97)
     parser.add_argument("--image", type=str, default=None)
-    parser.add_argument("--video", type=str, default=None)
+    parser.add_argument("--video", type=str, default=None) # 20250425 chaojie prompt travel & video input
     parser.add_argument("--ar_step", type=int, default=0)
     parser.add_argument("--causal_attention", action="store_true")
     parser.add_argument("--causal_block_size", type=int, default=1)
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     
     parser.add_argument(
         "--prompt",
-        nargs="+",
+        nargs="+", # 20250425 chaojie prompt travel & video input
         type=str,
         default="A woman in a leather jacket and sunglasses riding a vintage motorcycle through a desert highway at sunset, her hair blowing wildly in the wind as the motorcycle kicks up dust, with the golden sun casting long shadows across the barren landscape.",
     )
@@ -101,32 +101,48 @@ if __name__ == "__main__":
         args.image = resizecrop(args.image, height, width)
     image = args.image.convert("RGB") if args.image else None
     """
-    
-    #20250422 pftq: Add error handling for image loading, aspect ratio preservation
-    image = None
-    if args.image:
-        try:
-            image = load_image(args.image).convert("RGB")
 
+    # 20250425 chaojie prompt travel & video input
     video = []
-    pre_video_length = 17
-    if args.overlap_history is not None:
-        pre_video_length = args.overlap_history
     if args.video:
+        pre_video_length = 17
+        if args.overlap_history is not None:
+            pre_video_length = args.overlap_history
         args.video = load_video(args.video) 
         arg_width = width
         arg_height = height
         for img in args.video:
-            image_width, image_height = img.size
-            if image_height > image_width:
-                height, width = arg_width, arg_height
-            img = resizecrop(img, height, width)
+            # 20250422 pftq: option to preserve image aspect ratio
+            if args.preserve_image_aspect_ratio:
+                img_width, img_height = img.size
+                if img_height > img_width:
+                    height, width = width, height
+                    width = int(height / img_height * img_width)
+                else:
+                    height = int(width / img_width * img_height)
+        
+                divisibility=16
+                if width%divisibility!=0:
+                        width = width - (width%divisibility)
+                if height%divisibility!=0:
+                        height = height - (height%divisibility)
+        
+                img = resizecrop(img, height, width)
+            else:
+                image_width, image_height = img.size
+                if image_height > image_width:
+                    height, width = width, height
+                img = resizecrop(img, height, width)
             video.append(img.convert("RGB").resize((width, height)))
             video = video[-pre_video_length:]
     else:
         video = None
-    
-    negative_prompt = "色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走"
+
+        #20250422 pftq: Add error handling for image loading, aspect ratio preservation
+    image = None
+    if args.image and not args.video:
+        try:
+            image = load_image(args.image).convert("RGB")
 
             # 20250422 pftq: option to preserve image aspect ratio
             if args.preserve_image_aspect_ratio:
@@ -154,7 +170,7 @@ if __name__ == "__main__":
     
     #negative_prompt = "色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走"
     negative_prompt = args.negative_prompt # 20250422 pftq: allow editable negative prompt
-    
+  
     save_dir = os.path.join("result", args.outdir)
     os.makedirs(save_dir, exist_ok=True)
     local_rank = 0
@@ -188,7 +204,7 @@ if __name__ == "__main__":
         gc.collect()
         torch.cuda.empty_cache()
 
-    print(f"Rank {local_rank}: {width}x{height} | Image: "+str(image!=None))
+    print(f"Rank {local_rank}: {width}x{height} | Image Input: "+str(image!=None) + " | Video Input: "+str(video!=None))
     
     # 20250423 pftq: fixed 20-min load times on multi-GPU caused by contention, reduced down to 12 min roughly the same as single GPU.
     print("Initializing pipe at "+time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime()))
@@ -206,23 +222,16 @@ if __name__ == "__main__":
 
     if args.causal_attention:
         pipe.transformer.set_ar_attention(args.causal_block_size)
-
+    
     if args.teacache:
         if args.ar_step > 0:
-            num_steps = (
-                args.inference_steps
-                + (((args.base_num_frames - 1) // 4 + 1) // args.causal_block_size - 1) * args.ar_step
-            )
-            print("num_steps:", num_steps)
+            num_steps = args.inference_steps + (((args.base_num_frames - 1)//4 + 1) // args.causal_block_size - 1) * args.ar_step
+            print('num_steps:', num_steps)
         else:
             num_steps = args.inference_steps
-        pipe.transformer.initialize_teacache(
-            enable_teacache=True,
-            num_steps=num_steps,
-            teacache_thresh=args.teacache_thresh,
-            use_ret_steps=args.use_ret_steps,
-            ckpt_dir=args.model_id,
-        )
+        pipe.transformer.initialize_teacache(enable_teacache=True, num_steps=num_steps, 
+                                             teacache_thresh=args.teacache_thresh, use_ret_steps=args.use_ret_steps, 
+                                             ckpt_dir=args.model_id)
 
     #20250422 pftq: Set preferred linear algebra backend to avoid cuSOLVER issues
     torch.backends.cuda.preferred_linalg_library("default")  # or try "magma" if available
@@ -264,6 +273,21 @@ if __name__ == "__main__":
         np.random.seed(args.seed)
         torch.manual_seed(args.seed)
         torch.cuda.manual_seed_all(args.seed)
+
+
+        current_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+        #video_out_file = f"{args.prompt[:100].replace('/','')}_{args.seed}_{current_time}.mp4"
+        
+        # 20250422 pftq: more useful filename
+        gpucount = ""
+        if args.use_usp and dist.get_world_size():
+            gpucount = "_"+str(dist.get_world_size())+"xGPU"
+        prompt_summary = ""
+        if type(args.prompt) is list:
+            prompt_summary = args.prompt[0][:100].replace('/','')
+        else:
+            prompt_summary = args.prompt[:100].replace('/','')
+        video_out_file = f"{current_time}_skyreels2df_{args.resolution}-{args.num_frames}f_cfg{args.guidance_scale}_steps{args.inference_steps}_seed{args.seed}{gpucount}_{prompt_summary}_{idx}.mp4" 
         
         with torch.cuda.amp.autocast(dtype=pipe.transformer.dtype), torch.no_grad():
             video_frames = pipe(
@@ -283,48 +307,14 @@ if __name__ == "__main__":
                 ar_step=args.ar_step,
                 causal_block_size=args.causal_block_size,
                 fps=fps,
+
+                # 20250425 chaojie prompt travel & video input
+                video=video,
+                local_rank=local_rank,
+                save_dir=save_dir,
+                video_out_file=video_out_file,
             )[0]
     
         if local_rank == 0:
-            current_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
-            #video_out_file = f"{args.prompt[:100].replace('/','')}_{args.seed}_{current_time}.mp4"
-            
-            # 20250422 pftq: more useful filename
-            gpucount = ""
-            if args.use_usp and dist.get_world_size():
-                gpucount = "_"+str(dist.get_world_size())+"xGPU"
-            video_out_file = f"{current_time}_skyreels2df_{args.resolution}-{args.num_frames}f_cfg{args.guidance_scale}_steps{args.inference_steps}_seed{args.seed}{gpucount}_{args.prompt[:100].replace('/','')}_{idx}.mp4" 
-            
             output_path = os.path.join(save_dir, video_out_file)
             imageio.mimwrite(output_path, video_frames, fps=fps, quality=8, output_params=["-loglevel", "error"])
-    output_path = ""
-    current_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
-    video_out_file = f"{args.prompt[0][:100].replace('/','')}_{args.seed}_{current_time}.mp4"
-
-    with torch.cuda.amp.autocast(dtype=pipe.transformer.dtype), torch.no_grad():
-        video_frames = pipe(
-            prompt=prompt_input,
-            negative_prompt=negative_prompt,
-            image=image,
-            video=video,
-            height=height,
-            width=width,
-            num_frames=num_frames,
-            num_inference_steps=args.inference_steps,
-            shift=shift,
-            guidance_scale=guidance_scale,
-            generator=torch.Generator(device="cuda").manual_seed(args.seed),
-            overlap_history=args.overlap_history,
-            addnoise_condition=args.addnoise_condition,
-            base_num_frames=args.base_num_frames,
-            ar_step=args.ar_step,
-            causal_block_size=args.causal_block_size,
-            fps=fps,
-            local_rank=local_rank,
-            save_dir=save_dir,
-            video_out_file=video_out_file,
-        )[0]
-
-    if local_rank == 0:
-        output_path = os.path.join(save_dir, video_out_file)
-        imageio.mimwrite(output_path, video_frames, fps=fps, quality=8, output_params=["-loglevel", "error"])
